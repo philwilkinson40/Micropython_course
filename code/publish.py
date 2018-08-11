7d95087c8753481c66fbed4ca5e1858e8f756a86
Phil Wilko <philwilkinson40@gmail.com>
2018-08-11 17:42:22 +0800

    amended folder structure

--- code/publish.py
+++ code/publish.py
@@ -6,7 +6,7 @@ from umqtt.simple import MQTTClient
 i2c = I2C(scl=Pin(5), sda = Pin(4))
 sensor = dht12.DHT12(i2c)
 
-c=MQTTClient('phil_sensor', 'iot.eclipse.org')
+c=MQTTClient('phil_sensor1', 'iot.eclipse.org')
 
 while True:
     sensor.measure()
@@ -15,7 +15,7 @@ while True:
     print('temp is: ', temp)
     print('humidity is : ', humidity)
     c.connect()
-    c.publish('RIFF/phil/temperature', temp)
-    c.publish('RIFF/phil/humidity', humidity)
+    c.publish('RIFF/philip/temperature', str(temp))
+    c.publish('RIFF/philip/humidity', str(humidity))
     c.disconnect()
     time.sleep(10)
